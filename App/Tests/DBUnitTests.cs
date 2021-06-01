df98e4877d0882e84b2dd5802d90f32e85c48217
phobos42 <garrettruffner42@gmail.com>
2021-05-31 22:16:45 -0400

    🧪 Added Some Tests

--- App/Tests/DBUnitTests.cs
+++ App/Tests/DBUnitTests.cs
@@ -1,11 +1,16 @@
 using System.Net.Mail;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using StoreModels;
 using Data;
+
 
 using Xunit;
 
 using System.Collections.Generic;
+using Moq;
+using System;
+using Service;
 
 namespace Tests
 {
@@ -15,68 +20,46 @@ namespace Tests
         public DBUnitTests(){
             options = new DbContextOptionsBuilder<StoreDBContext>().UseSqlite("Filename=Test.db").Options;
         }
-        [Fact]
-        //Tests the retrival of all customers as well as adding a new customer
-        public void TestCustomersBeingAdded()
-        {
-        //    Customer c = new Customer("Billy Joe", "123 Street", "asdf@somewher.net");
-        //     using (var context = new StoreDBContext(options)){
-        //         context.Database.EnsureDeleted();
-        //         context.Database.EnsureCreated();
-        //         IRepository _repo = new RepoDB(context);
-        //         _repo.AddCustomer(c);
-        //         context.SaveChanges();
-           
-        //     }
-        //     //Using a new context check for customer
-        //     using (var context = new StoreDBContext(options)){
-        //         IRepository _repo = new RepoDB(context);
-        //         List<Customer> cs = _repo.GetAllCustomers();
-        //         Customer cdb = cs.ToArray()[0];
-        //         Assert.True(cs.Count == 1);
-        //         Assert.Equal(c.Name, cdb.Name);
-        //         Assert.Equal(c.Address, cdb.Address);
-        //         Assert.Equal(c.Email, cdb.Email);
-        //         context.Database.EnsureDeleted();
 
-        //    }
-        }
         [Fact]
         //Tests the retrival of all locations as well as adding a new location
         public void TestLocationBeingAdded()
         {
-        //    Location c = new Location("My Location", "123 Peanut Street, Nowhere, TX 39849");
-        //     using (var context = new StoreDBContext(options)){
-        //         context.Database.EnsureDeleted();
-        //         context.Database.EnsureCreated();
-        //         IRepository _repo = new RepoDB(context);
-        //         _repo.AddLocation(c);
-        //         context.SaveChanges();
+           Location c = new Location(){LocationName = "name", Address="address"};
+            using (var context = new StoreDBContext(options)){
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                IRepository _repo = new RepoDB(context);
+                _repo.AddLocation(c);
+                
            
-        //     }
-        //     //Using a new context check for Location
-        //     using (var context = new StoreDBContext(options)){
-        //         IRepository _repo = new RepoDB(context);
-        //         List<Location> cs = _repo.GetAllLocations();
-        //         Location cdb = cs.ToArray()[0];
-        //         Assert.Equal(c.LocationName, cdb.LocationName);
-        //         Assert.Equal(c.Address, cdb.Address);
-        //         Assert.True(cs.Count == 1);
-        //         context.Database.EnsureDeleted();
+            }
+            //Using a new context check for Location
+            using (var context = new StoreDBContext(options)){
+                IRepository _repo = new RepoDB(context);
+                List<Location> cs = _repo.GetAllLocations();
+                Location cdb = cs.ToArray()[0];
+                Assert.Equal(c.LocationID, cdb.LocationID);
+                Assert.Equal(c.LocationName, cdb.LocationName);
+                Assert.Equal(c.Address, cdb.Address);
+                Assert.True(cs.Count == 1);
+                context.Database.EnsureDeleted();
 
-        //    }
+           }
         }
         [Fact]
         //Tests the retrival of all products as well as adding a new product
         public void TestProductBeingAdded()
         {
+            // var mockRepo = new Mock<IRepository>();
+
             Product c = new Product("My Product", 12.99);
             using (var context = new StoreDBContext(options)){
                 context.Database.EnsureDeleted();
                 context.Database.EnsureCreated();
                 IRepository _repo = new RepoDB(context);
                 _repo.AddProduct(c);
-                context.SaveChanges();
+                
            
             }
             //Using a new context check for Product
@@ -87,9 +70,53 @@ namespace Tests
                 Assert.Equal(c.Name, cdb.Name);
                 Assert.Equal(c.Price, cdb.Price);
                 Assert.True(cs.Count == 1);
+                Assert.Equal(c.ProductID, cdb.ProductID);
                 context.Database.EnsureDeleted();
 
            }
+
+        }
+
+        [Fact]
+        public void AddToInventoryTest(){
+            //Create Location
+
+            Location c = new Location(){LocationName = "name", Address="address"};
+            using (var context = new StoreDBContext(options)){
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                IServices service = new Services(new RepoDB(context), null);
+                
+                service.AddLocation("name", "alksdjf", new Guid());
+                service.AddProduct("my product", 10);               
+            }
+            
+            //Create Product
+            Product p = new Product("My Product", 12.99);
+            using (var context = new StoreDBContext(options)){
+            IServices service = new Services(new RepoDB(context), null);
+               var products =  service.GetAllProducts();
+               var locations = service.GetAllLocations();
+               Assert.NotEmpty(locations);
+               Assert.True(locations.Count ==1);
+               Assert.True(1==products.Count);
+               //get location and product ID
+               int pid = products[0].ProductID;
+               int lid = locations[0].LocationID;
+
+               service.AddProductToInventory(lid,pid, 0);
+               
+            }
+
+            using (var context = new StoreDBContext(options)){
+                IServices service = new Services(new RepoDB(context), null); 
+                var locations = service.GetAllLocations();
+                Assert.True(1 == locations[0].InventoryItems.Count);
+            }
+
+           
+
+
 
         }
         
--- App/Tests/ValidationUnitTests.cs
+++ App/Tests/ValidationUnitTests.cs
@@ -1,125 +1,174 @@
 using System;
 using Xunit;
-using VS = Service.ValidationService;
+using StoreModels;
+
 
 namespace Tests
 {
     public class ValidationUnitTests
     {
-        [Fact]
-        public void CityValidationTest()
-        {
-            Assert.True(VS.ValidateCityName("St. Louis"));
-            Assert.False(VS.ValidateCityName("city #4"));
-            Assert.False(VS.ValidateAddress(" "));
-            Assert.False(VS.ValidateAddress(""));
-        }
-        [Fact]
-        public void NameValidationTest()
-        {
-            Assert.True(VS.ValidatePersonName("Daniel McDowell"));
-            Assert.False(VS.ValidatePersonName("sk8r boi"));
-            Assert.False(VS.ValidatePersonName(" "));
-            Assert.False(VS.ValidatePersonName(""));
-        }
-        [Fact]
-        public void AddressValidationTest()
-        {
-            Assert.True(VS.ValidateAddress("8323 Somewhere Drive"));
-            Assert.True(VS.ValidateAddress("2938 Apt. #289, place, city 20399"));
-            Assert.False(VS.ValidateAddress("asfdVDKL3893e(*#@*"));
-            Assert.False(VS.ValidateAddress(" "));
-            Assert.False(VS.ValidateAddress(""));
-        }
-        [Fact]
-        public void DoubleValidatioTest()
-        {
-            Assert.True(VS.ValidateDouble("39.40"));
-            Assert.True(VS.ValidateDouble("-23.34"));
-            Assert.True(VS.ValidateDouble("-8"));
-            Assert.True(VS.ValidateDouble("70"));
-            Assert.False(VS.ValidateDouble(" "));
-            Assert.False(VS.ValidateDouble(""));
-        }
-        [Fact]
-        public void StringValidationTest()
-        {
-            Assert.True(VS.ValidateString("lskdfjl238 ei328*(#@* i2398"));
-            Assert.False(VS.ValidateString(" "));
-            Assert.False(VS.ValidateString(""));
+        // Test Models to make sure they only take good data
 
+        [Fact]
+        public void PriceValidationTest(){
+            Product p = new Product();
+            Assert.Throws<Exception>(() =>p.Price = -5.05);
         }
         [Fact]
-        public void ValidtateAddressTest()
-        {
-            Assert.True(VS.ValidateAddress("LKsdfjeie#Apt93392,-,     stuff29"));
-            Assert.False(VS.ValidateAddress("!@$%&*(){}[]\\_=|+?"));
-            Assert.False(VS.ValidateAddress(" "));
-            Assert.False(VS.ValidateAddress(""));
+        public void QuantityValidationTest(){
+            InventoryItem ii = new InventoryItem();
+            Assert.Throws<Exception>(() =>ii.Quantity = -5);
         }
         [Fact]
-        public void ValidateIntTest()
-        {
-            Assert.True(VS.ValidateInt("189239829813"));
-            Assert.True(VS.ValidateInt("-189239829813"));
-            Assert.True(VS.ValidateInt("-0000189239829813"));
-            Assert.True(VS.ValidateInt("+00189239829813"));
-            Assert.False(VS.ValidateInt("+189*239829813"));
-            Assert.False(VS.ValidateInt("^129"));
-            Assert.False(VS.ValidateInt("+"));
-            Assert.False(VS.ValidateInt("-"));
-            Assert.False(VS.ValidateInt(" "));
-            Assert.False(VS.ValidateInt(""));
+        public void QuantityValidationTest2(){
+            OrderItem ii = new OrderItem();
+            Assert.Throws<Exception>(() =>ii.Quantity = -5);
         }
+
         [Fact]
-        public void ValidatPositiveIntTest()
-        {
-            Assert.True(VS.ValidatePositiveInt("189239829813"));
-            Assert.True(VS.ValidatePositiveInt("+00189239829813"));
-            Assert.False(VS.ValidatePositiveInt("+"));
-            Assert.False(VS.ValidatePositiveInt("-"));
-            Assert.False(VS.ValidatePositiveInt("+189*239829813"));
-            Assert.False(VS.ValidatePositiveInt("^129"));
-            Assert.False(VS.ValidatePositiveInt("-189239829813"));
-            Assert.False(VS.ValidatePositiveInt("-0000189239829813"));
-            Assert.False(VS.ValidatePositiveInt(" "));
-            Assert.False(VS.ValidatePositiveInt(""));
+        public void ProductNameValidationTest(){
+            Product p = new Product();
+            Assert.Throws<Exception>(() =>p.Name = "");
         }
-        [Fact]
-        public void ValidateNegativeIntTest()
+
+        [Theory]
+        [InlineData ("name" ,-1.10)]
+        [InlineData ("naem",-0.00001)]
+        [InlineData ("",5)]
+        [InlineData ("   ",5)]
+
+        public void ProductPriceTest(string name, double price)
         {
-            Assert.True(VS.ValidateNegativeInt("-189239829813"));
-            Assert.True(VS.ValidateNegativeInt("-00189239829813"));
-            Assert.False(VS.ValidateNegativeInt("+"));
-            Assert.False(VS.ValidateNegativeInt("-"));
-            Assert.False(VS.ValidateNegativeInt("+189*239829813"));
-            Assert.False(VS.ValidateNegativeInt("^129"));
-            Assert.False(VS.ValidateNegativeInt("+189239829813"));
-            Assert.False(VS.ValidateNegativeInt("0000189239829813"));
-            Assert.False(VS.ValidateNegativeInt(" "));
-            Assert.False(VS.ValidateNegativeInt(""));
+            
+            Assert.Throws<Exception>(() => new Product(name, price));
         }
-        [Fact]
-        public void ValidateIntWithinRangeTest()
+        [Theory]
+        [InlineData (-10)]
+        [InlineData (-5)]
+        public void ItemQuantityTest(int quantity)
         {
-            Assert.True(VS.ValidateIntWithinRange("01", 0,10));
-            Assert.True(VS.ValidateIntWithinRange("1", 0,1));
-            Assert.True(VS.ValidateIntWithinRange("0", 0,1));
-            Assert.True(VS.ValidateIntWithinRange("-10", -11,1));
-            Assert.False(VS.ValidateIntWithinRange("-10", 0,10));
-            Assert.False(VS.ValidateIntWithinRange("", 0,1));
-            Assert.False(VS.ValidateIntWithinRange(" ", 0,1));
+            OrderItem oi = new OrderItem();
+            Assert.Throws<Exception>(() =>oi.Quantity = quantity);
+            InventoryItem ii = new InventoryItem();
+            Assert.Throws<Exception>(() =>ii.Quantity = quantity);
         }
-        [Fact]
-        public void ValidateEmailTest()
-        {
-            Assert.True(VS.ValidateEmail("asdf@asdf.asdf"));
-            Assert.True(VS.ValidateEmail("101@a.b"));
-            Assert.False(VS.ValidateEmail("@lkasjdff.sdkf.sdf"));
-            Assert.False(VS.ValidateEmail("a@lkasjdf@...83f.sdkf.sdf.0"));
-            Assert.False(VS.ValidateEmail(" "));
-            Assert.False(VS.ValidateEmail(""));
-        }
-        
+
+
     }
 }
+        // [Fact]
+        // public void CityValidationTest()
+        // {
+        //     Assert.True(VS.ValidateCityName("St. Louis"));
+        //     Assert.False(VS.ValidateCityName("city #4"));
+        //     Assert.False(VS.ValidateAddress(" "));
+        //     Assert.False(VS.ValidateAddress(""));
+        // }
+        // [Fact]
+        // public void NameValidationTest()
+        // {
+        //     Assert.True(VS.ValidatePersonName("Daniel McDowell"));
+        //     Assert.False(VS.ValidatePersonName("sk8r boi"));
+        //     Assert.False(VS.ValidatePersonName(" "));
+        //     Assert.False(VS.ValidatePersonName(""));
+        // }
+        // [Fact]
+        // public void AddressValidationTest()
+        // {
+        //     Assert.True(VS.ValidateAddress("8323 Somewhere Drive"));
+        //     Assert.True(VS.ValidateAddress("2938 Apt. #289, place, city 20399"));
+        //     Assert.False(VS.ValidateAddress("asfdVDKL3893e(*#@*"));
+        //     Assert.False(VS.ValidateAddress(" "));
+        //     Assert.False(VS.ValidateAddress(""));
+        // }
+        // [Fact]
+        // public void DoubleValidatioTest()
+        // {
+        //     Assert.True(VS.ValidateDouble("39.40"));
+        //     Assert.True(VS.ValidateDouble("-23.34"));
+        //     Assert.True(VS.ValidateDouble("-8"));
+        //     Assert.True(VS.ValidateDouble("70"));
+        //     Assert.False(VS.ValidateDouble(" "));
+        //     Assert.False(VS.ValidateDouble(""));
+        // }
+        // [Fact]
+        // public void StringValidationTest()
+        // {
+        //     Assert.True(VS.ValidateString("lskdfjl238 ei328*(#@* i2398"));
+        //     Assert.False(VS.ValidateString(" "));
+        //     Assert.False(VS.ValidateString(""));
+
+        // }
+        // [Fact]
+        // public void ValidtateAddressTest()
+        // {
+        //     Assert.True(VS.ValidateAddress("LKsdfjeie#Apt93392,-,     stuff29"));
+        //     Assert.False(VS.ValidateAddress("!@$%&*(){}[]\\_=|+?"));
+        //     Assert.False(VS.ValidateAddress(" "));
+        //     Assert.False(VS.ValidateAddress(""));
+        // }
+        // [Fact]
+        // public void ValidateIntTest()
+        // {
+        //     Assert.True(VS.ValidateInt("189239829813"));
+        //     Assert.True(VS.ValidateInt("-189239829813"));
+        //     Assert.True(VS.ValidateInt("-0000189239829813"));
+        //     Assert.True(VS.ValidateInt("+00189239829813"));
+        //     Assert.False(VS.ValidateInt("+189*239829813"));
+        //     Assert.False(VS.ValidateInt("^129"));
+        //     Assert.False(VS.ValidateInt("+"));
+        //     Assert.False(VS.ValidateInt("-"));
+        //     Assert.False(VS.ValidateInt(" "));
+        //     Assert.False(VS.ValidateInt(""));
+        // }
+        // [Fact]
+        // public void ValidatPositiveIntTest()
+        // {
+        //     Assert.True(VS.ValidatePositiveInt("189239829813"));
+        //     Assert.True(VS.ValidatePositiveInt("+00189239829813"));
+        //     Assert.False(VS.ValidatePositiveInt("+"));
+        //     Assert.False(VS.ValidatePositiveInt("-"));
+        //     Assert.False(VS.ValidatePositiveInt("+189*239829813"));
+        //     Assert.False(VS.ValidatePositiveInt("^129"));
+        //     Assert.False(VS.ValidatePositiveInt("-189239829813"));
+        //     Assert.False(VS.ValidatePositiveInt("-0000189239829813"));
+        //     Assert.False(VS.ValidatePositiveInt(" "));
+        //     Assert.False(VS.ValidatePositiveInt(""));
+        // }
+        // [Fact]
+        // public void ValidateNegativeIntTest()
+        // {
+        //     Assert.True(VS.ValidateNegativeInt("-189239829813"));
+        //     Assert.True(VS.ValidateNegativeInt("-00189239829813"));
+        //     Assert.False(VS.ValidateNegativeInt("+"));
+        //     Assert.False(VS.ValidateNegativeInt("-"));
+        //     Assert.False(VS.ValidateNegativeInt("+189*239829813"));
+        //     Assert.False(VS.ValidateNegativeInt("^129"));
+        //     Assert.False(VS.ValidateNegativeInt("+189239829813"));
+        //     Assert.False(VS.ValidateNegativeInt("0000189239829813"));
+        //     Assert.False(VS.ValidateNegativeInt(" "));
+        //     Assert.False(VS.ValidateNegativeInt(""));
+        // }
+        // [Fact]
+        // public void ValidateIntWithinRangeTest()
+        // {
+        //     Assert.True(VS.ValidateIntWithinRange("01", 0,10));
+        //     Assert.True(VS.ValidateIntWithinRange("1", 0,1));
+        //     Assert.True(VS.ValidateIntWithinRange("0", 0,1));
+        //     Assert.True(VS.ValidateIntWithinRange("-10", -11,1));
+        //     Assert.False(VS.ValidateIntWithinRange("-10", 0,10));
+        //     Assert.False(VS.ValidateIntWithinRange("", 0,1));
+        //     Assert.False(VS.ValidateIntWithinRange(" ", 0,1));
+        // }
+        // [Fact]
+        // public void ValidateEmailTest()
+        // {
+        //     Assert.True(VS.ValidateEmail("asdf@asdf.asdf"));
+        //     Assert.True(VS.ValidateEmail("101@a.b"));
+        //     Assert.False(VS.ValidateEmail("@lkasjdff.sdkf.sdf"));
+        //     Assert.False(VS.ValidateEmail("a@lkasjdf@...83f.sdkf.sdf.0"));
+        //     Assert.False(VS.ValidateEmail(" "));
+        //     Assert.False(VS.ValidateEmail(""));
+        // }
+        
+
